4b4ea82d5f0b680dcde6b293cd6ac14d374309ff
mia <miablidberg@gmail.com>
2026-09-11 14:43:54 +0200

    Add Interface for Location Service

---
+++ ITAssetManagement.Application/Locations/Services/ILocationService.cs
@@ -0,0 +1,7 @@
+﻿namespace ITAssetManagement.Application.Locations.Services;
+
+internal interface ILocationService
+{
+    CreateLocationResponse CreateCustomer(CreateCustomerRequest request);
+    GetAllLocationsResponse GetAllLocations();
+}
--- ITAssetManager.Domain/Locations/Models/Location.cs
+++ ITAssetManager.Domain/Locations/Models/Location.cs
@@ -2,13 +2,13 @@
 
 namespace ITAssetManager.Domain.Locations.Models;
 
-internal class Location(Guid locationId, LocationCode locationCode, LocationName locationName)
+public class Location(Guid locationId, LocationCode locationCode, LocationName locationName)
 {
     public Guid LocationId { get; init; } = locationId;
     public LocationCode LocationCode { get; init; } = locationCode;
     public LocationName LocationName { get; private set; } = locationName;
 
-    public void ChangeLocationName(LocationName locationName)
+    public void Rename(LocationName locationName)
     {
         LocationName = locationName;
     }
