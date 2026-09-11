22b0087041011c14563abed45768d5a89c4de3c9
Gustav Andreasson <Gustav.Andreasson@ekebacken.se>
2026-09-11 14:29:08 +0200

    Add interface for license service

---
+++ ITAssetManagement.Application/Licenses/ILicenseService.cs
@@ -0,0 +1,7 @@
+﻿namespace ITAssetManagement.Application.Licenses;
+
+public interface ILicenseService
+{
+    CreateLicenseRespose CreateLicense(CreateLicenseRequest request);
+    GetLicenseResponse GetAllLicenses();
+}
