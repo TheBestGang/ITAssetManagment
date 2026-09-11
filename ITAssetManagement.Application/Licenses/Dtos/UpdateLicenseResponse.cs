552b209ddb14a6f66ceec7ac5853541f2fe08fbe
Gustav Andreasson <Gustav.Andreasson@ekebacken.se>
2026-09-11 14:37:31 +0200

    Add response models

---
+++ ITAssetManagement.Application/Licenses/Dtos/UpdateLicenseResponse.cs
@@ -0,0 +1,11 @@
+﻿using ITAssetManager.Domain.Licenses.Models;
+using ITAssetManager.Domain.Licenses.ValueObjects;
+
+namespace ITAssetManagement.Application.Licenses.Dtos;
+
+public record UpdateLicenseResponse
+(
+    bool Success,
+    SeatCount SeatCount,
+    string? ErrorMessage
+    );
