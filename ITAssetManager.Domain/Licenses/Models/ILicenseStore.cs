3ffb54f5ab2a0a749a7948d6ca4c1ff3b6f0dcbc
Gustav Andreasson <Gustav.Andreasson@ekebacken.se>
2026-09-11 16:19:28 +0200

    Add in-memory license store

---
+++ ITAssetManager.Domain/Licenses/Models/ILicenseStore.cs
@@ -0,0 +1,11 @@
+﻿using ITAssetManager.Domain.Licenses.ValueObjects;
+
+namespace ITAssetManager.Domain.Licenses.Models;
+
+public interface ILicenseStore
+{
+    bool AddLicense(License license);
+    IReadOnlyList<License> GetAllLicenses();
+    bool UpdateLicense(string licenseId, SeatCount newSeatCount, out SeatCount seatCount);
+    License GetLicenseByLicenseId(string licenseId, out License license);
+}
---
+++ ITAssetManagment.Infrastructure/Licenses/Persistance/InMemory/InMemoryLicenseStore.cs
@@ -0,0 +1,46 @@
+﻿using ITAssetManager.Domain.Licenses.Models;
+using ITAssetManager.Domain.Licenses.ValueObjects;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace ITAssetManagment.Infrastructure.Licenses.Persistance.InMemory;
+
+
+internal class InMemoryLicenseStore : ILicenseStore
+{
+    public readonly List<License> _licenses = [];
+
+    public bool AddLicense(License license)
+    {
+        ArgumentNullException.ThrowIfNull(license);
+        _licenses.Add(license);
+        return true;
+    }
+
+    public IReadOnlyList<License> GetAllLicenses()
+    {
+        return _licenses;
+    }
+
+    public bool UpdateLicense(string licenseId, SeatCount newSeatCount, out SeatCount seatCount)
+    {
+        GetLicenseByLicenseId(licenseId, out var license);
+        if (license == null)
+        {
+            seatCount = null;
+            return false;
+        }
+        var index = _licenses.IndexOf(license);
+        if (index > -1)
+        {
+            _licenses[index].SeatCount = newSeatCount;
+        }
+        seatCount = newSeatCount;
+        return true;
+    }
+    public License GetLicenseByLicenseId(string licenseId, out License license)
+    {
+        ArgumentNullException.ThrowIfNull(licenseId, nameof(licenseId));
+        license = _licenses.FirstOrDefault(x => x.LicenseId == licenseId);
+        return license;
+    }
+}
