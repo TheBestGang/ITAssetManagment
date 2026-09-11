9d6f9a4c7f5522df2458c13700ffac12b92a86fe
mia <miablidberg@gmail.com>
2026-09-11 14:17:54 +0200

    Add base of Location Model

---
+++ ITAssetManager.Domain/Locations/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿namespace ITAssetManager.Domain.Locations.Models;
+
+internal class Location(Guid locationId, string locationCode, string locationName)
+{
+    public Guid LocationId { get; set; } = locationId;
+    public string LocationCode { get; init; } = locationCode;
+    public string LocationName { get; set; } = locationName;
+
+    public void ChangeLocationName(string locationName)
+    {
+        LocationName = locationName;
+    }
+}
+
+
