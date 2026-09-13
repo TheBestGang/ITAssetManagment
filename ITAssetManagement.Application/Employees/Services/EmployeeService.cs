9777562cbf5f0f0af50833169dad15ec97fa842b
lilyboyce96 <lily.esther.boyce@gmail.com>
2026-09-13 23:20:13 +0200

    Add employee service interface

---
+++ ITAssetManagement.Application/Employees/Services/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using ITAssetManagement.Application.Employees.Interfaces;
+using ITAssetManager.Domain.Employees.Models;
+using ITAssetManager.Domain.Employees.Interface;
+
+namespace ITAssetManagement.Application.Employees.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+       public readonly IEmployeeRepository employeeRepository;
+        public void DeactivateEmployee(Guid employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterEmployee(string employeeName, string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ITAssetManager.Domain/Employees/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using ITAssetManager.Domain.Employees.Models;
+
+namespace ITAssetManager.Domain.Employees.Interface
+{
+    public interface IEmployeeRepository
+    {
+        void Add(Employee employee);
+
+        IReadOnlyList<Employee> GetAll();
+
+        Employee? GetById(Guid employeeId);
+
+        Employee? GetByEmail(string emailAddress);
+
+        void Update(Employee employee);
+    }
+}
